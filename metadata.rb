c419e31c244532587c4a7592f2137f200309ef06
Ivan Suftin <isuftin@usgs.gov>
2016-06-16 13:27:58 -0500

    Added ChefSpec and ServerSpec testing

--- metadata.rb
+++ metadata.rb
@@ -6,5 +6,7 @@ description      'Wrapper cookbook around the community Consul cookbook'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.0.1'
 
-source_url 'https://github.com/johnbellone/consul-cookbook' if respond_to?(:source_url)
-issues_url 'https://github.com/johnbellone/consul-cookbook/issues' if respond_to?(:issues_url)
+source_url 'https://github.com/USGS-CIDA/chef-cookbook-consul' if respond_to?(:source_url)
+issues_url 'https://github.com/USGS-CIDA/chef-cookbook-consul/issues' if respond_to?(:issues_url)
+
+depends 'consul'
